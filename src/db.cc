3e122d0369f172d08f777d0c7dca329a5c485595
Antonio Borondo <antonioborondo@outlook.com>
2023-12-21 14:10:19 +0100

    Format code

--- src/db.cc
+++ src/db.cc
@@ -4,9 +4,7 @@ Db::Db()
 {
     sqlite3_open("sw.db", &db_);
 
-    std::string sql{
-        "CREATE TABLE IF NOT EXISTS log (timestamp TEXT PRIMARY KEY NOT NULL "
-        "DEFAULT(strftime('%Y-%m-%d %H:%M:%f', 'now', 'localtime')), type INTEGER NOT NULL)"};
+    std::string sql{"CREATE TABLE IF NOT EXISTS log (timestamp TEXT PRIMARY KEY NOT NULL DEFAULT(strftime('%Y-%m-%d %H:%M:%f', 'now', 'localtime')), type INTEGER NOT NULL)"};
     sqlite3_exec(db_, sql.c_str(), nullptr, nullptr, nullptr);
 }
 
@@ -17,8 +15,7 @@ Db::~Db()
 
 void Db::Log(LogType log_type)
 {
-    std::string sql{
-        "INSERT INTO log (type) VALUES(" + std::to_string(static_cast<int>(log_type)) + ")"};
+    std::string sql{"INSERT INTO log (type) VALUES(" + std::to_string(static_cast<int>(log_type)) + ")"};
     sqlite3_exec(db_, sql.c_str(), nullptr, nullptr, nullptr);
 }
 
