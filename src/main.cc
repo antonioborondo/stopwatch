7840bd73295388bab48dccfe90475fe3f83fb700
Antonio Borondo <antonioborondo@users.noreply.github.com>
2023-12-21 19:18:30 +0100

    Rename (#3)

--- src/main.cc
+++ src/main.cc
@@ -13,9 +13,9 @@ int main(int argc, char** argv)
         po::options_description options_description;
         // clang-format off
         options_description.add_options()
-        ("login,i", "record login timestamp")
-        ("logout,o", "record logout timestamp")
-        ("summary,s", "display summary")
+        ("start", "start time tracker")
+        ("stop", "stop time tracker")
+        ("summary", "display time tracker summary")
         ("help", "display this help and exit")
         ("version", "output version information and exit")
         ;
@@ -24,31 +24,31 @@ int main(int argc, char** argv)
         po::variables_map variables_map;
         po::store(po::parse_command_line(argc, argv, options_description), variables_map);
 
-        if(variables_map.count("login"))
+        if(variables_map.count("start"))
         {
-            std::cout << "Login" << std::endl;
+            std::cout << "Start time tracker" << std::endl;
             Db db;
             db.Log(Db::LogType::kIn);
         }
-        else if(variables_map.count("logout"))
+        else if(variables_map.count("stop"))
         {
-            std::cout << "Logout" << std::endl;
+            std::cout << "Stop time tracker" << std::endl;
             Db db;
             db.Log(Db::LogType::kOut);
         }
         else if(variables_map.count("summary"))
         {
-            std::cout << "Summary:" << std::endl;
+            std::cout << "Time tracker summary:" << std::endl;
             Db db;
             std::cout << db.Summary() << " hours" << std::endl;
         }
         else if(variables_map.count("version"))
         {
-            std::cout << "1.0.0" << std::endl;
+            std::cout << "time_tracker 1.0.0" << std::endl;
         }
         else
         {
-            std::cout << "Usage: sw [OPTION]" << std::endl;
+            std::cout << "Usage: time_tracker [OPTION]" << std::endl;
             std::cout << options_description << std::endl;
         }
     }
